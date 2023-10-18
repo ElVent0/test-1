5cbda34026ec6789e9188d6ac5b7897cb40fd7ec
Vas <vasylviter95@gmail.com>
2023-10-18 20:33:23 +0300

    Done

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 import { companyArray } from "./companyArray.js";
-import { isThisTitle } from "./isThisTitle.js";
 import { isInParams } from "./isInParams.js";
 
-export { companyArray, isThisTitle, isInParams };
+export { companyArray, isInParams };
